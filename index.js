afab22ba491dae6336030652cab956c9574037b1
psandesh64 <psandesh64@gmail.com>
2024-01-06 09:19:42 +0545

    getting started on Express

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
-const http = require('http')
-
-let notes = [
-    {
-    id:1,
-    content: "HTML is easy.",
-    important: true,
-    },
-    {
-    id:2,
-    content: "Browser can execute only javascript.",
-    important: true,
-    },
-    {
-    id:3,
-    content: "GET and POST are the most important method of HTTP protocol.",
-    important: true,
-    },  
-]
-
-const app = http.createServer((request,response) => {
-    response.writeHead(200, { 'Content-type':'application/json' })
-    response.end(JSON.stringify(notes))
-})
+const express = require('express')
+const app = express()
 const PORT = 3001
-app.listen(PORT)
 
-console.log(`Server running on port localhost/${PORT}`)
+app.use(express.json())
+let students = [
+    {
+        id : 1,
+        name : 'Bryan',
+        age : 24
+    },
+    {
+        id : 2,
+        name : 'Saphire',
+        age : 29
+    },
+    {
+        id : 3,
+        name : 'Koran',
+        age : 25
+    },
+]
+const requestLogger = (request,response,next) => {
+    console.log('Method',request.method);
+    console.log('Path',request.path);
+    console.log('Body',request.body);
+    console.log('----------------------');
+    next();
+}
+app.use(requestLogger)
+
+const unknownEndpoint = (request,response) => {
+    response.status(404).send({error:'unknown endpoint'})
+}
+app.use(unknownEndpoint)
+
+app.get('/',(request,response)=>{
+    response.send('<h1>This is my home page</h1>');
+})
+app.get('/students',(request,response) => {
+    response.json(students)
+})
+app.get('/students/:id',(request,response) => {
+    const id = Number(request.params.id)
+    const student = students.find( data => data.id === id )
+    student ? response.json(student) : response.status(404).end()
+})
+app.delete('/students/:id',(request,response) => {
+    const id = Number(request.params.id)
+    students = students.filter(data => data.id !== id)
+    response.status(204).end()
+})
+app.post('/students',(request,response) => {
+    const student = request.body
+    students.push(student)
+    response.json(student)
+})
+
+app.listen(PORT,() => console.log(`Server running on localhost:${PORT}`))
