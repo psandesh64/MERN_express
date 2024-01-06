5d59e140eab3ebc5973c260350e056522368113f
psandesh64 <psandesh64@gmail.com>
2024-01-06 16:15:02 +0545

    second commit

--- index.js
+++ index.js
@@ -37,18 +37,15 @@ const unknownEndpoint = (request,response) => {
 app.get('/',(request,response)=>{
     response.send('<h1>This is my home page</h1>');
 })
+
 app.get('/api/persons',(request,response) => {
     response.json(phonebook)
 })
+
 app.get('/api/persons/:id',(request,response) => {
     const id = Number(request.params.id)
     const person = phonebook.find( data => data.id === id )
     student ? response.json(person) : response.status(404).end()
-})
-app.delete('/api/persons/:id',(request,response) => {
-    const id = Number(request.params.id)
-    phonebook = phonebook.filter(data => data.id !== id)
-    response.status(204).end()
 })
 
 app.use(unknownEndpoint)
